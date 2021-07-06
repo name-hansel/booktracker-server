2754b030460abe1a32ae4d39a4209d632f2a5ffd
name-hansel <snehal6mane@gmail.com>
2021-07-06 18:30:30 +0530

    Verify account and login

--- routes/auth.ts
+++ routes/auth.ts
@@ -2,13 +2,17 @@ import * as express from "express";
 const router = express.Router();
 import bcrypt from "bcrypt";
 import crypto from "crypto";
+import jwt from "jsonwebtoken";
 
 import { RegisterUser } from "../interfaces";
-import { registrationValidation } from "../utils/validation";
+import { registrationValidation, loginValidation } from "../utils/validation";
 import User from "../models/User";
 import redis from "../config/redis";
 import { sendEmail } from "../utils/nodemailer";
 
+// @route   POST /auth/register
+// @desc    Create new account
+// @access  Public
 router.post("/register", async (req, res) => {
   const { email, username, password }: RegisterUser = req.body;
 
@@ -20,18 +24,18 @@ router.post("/register", async (req, res) => {
     });
 
   try {
+    // Check if username exists
+    const usernameExists = await User.findOne({ username });
+    if (usernameExists)
+      return res.status(400).send({
+        error: "Please choose another username",
+      });
+
     // Check if email exists
     const emailExists = await User.findOne({ email });
     if (emailExists)
       return res.status(400).send({
         error: "Some error has occurred",
-      });
-
-    // Check if username exists
-    const usernameExists = await User.findOne({ username });
-    if (usernameExists)
-      return res.status(400).send({
-        message: "Please choose another username",
       });
 
     // Hash password
@@ -56,7 +60,7 @@ router.post("/register", async (req, res) => {
     sendEmail(email, emailHash);
 
     // Save the hash in redis along with userid
-    await redis.set(emailHash, _id.toString());
+    await redis.set(emailHash, _id.toString(), "ex", 60 * 60 * 24);
 
     return res.status(200).json({
       id: _id,
@@ -72,13 +76,101 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// @route   GET /auth/verify/:hash
+// @desc    Verify account
+// @access  Public
 router.get("/verify/:hash", async (req, res) => {
   const hash = req.params.hash;
   try {
-    const id = await redis.get(hash);
-    // id is userid
+    const id = await redis.get(hash.toString());
+    if (!id)
+      return res.status(400).json({
+        error: "Account not found",
+      });
+
+    await User.findOneAndUpdate(
+      { _id: id },
+      { activated: true },
+      { new: true }
+    );
+    await redis.del(hash);
+
+    return res.status(200).json({
+      message: "Account has been activated",
+    });
   } catch (err) {
     console.error(err.message);
+    return res.status(500).json({
+      error: "Server error",
+    });
+  }
+});
+
+// @route   POST /auth/login
+// @desc    Login user, send access token and set cookie
+// @access  Public
+router.post("/login", loginValidation, async (req, res) => {
+  try {
+    // Get data from form
+    const finder = req.body.email
+      ? { key: "email", value: req.body.email }
+      : { key: "username", value: req.body.username };
+    const password = req.body.password;
+
+    // Find user in database by email or username
+    const user =
+      finder.key === "email"
+        ? await User.findOne({ email: finder.value })
+        : await User.findOne({ username: finder.value });
+
+    // No user with those credentials exists
+    if (!user) return res.status(400).json({ error: "Invalid credentials" });
+
+    if (!user.activated)
+      return res.status(401).json({ error: "Account has not been activated." });
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) return res.status(400).json({ error: "Invalid credentials" });
+
+    const payload = {
+      user_id: user._id,
+    };
+
+    const accessToken = await jwt.sign(
+      payload,
+      process.env.ACCESS_TOKEN_SECRET!,
+      {
+        expiresIn: "6h",
+      }
+    );
+
+    const refreshToken = await jwt.sign(
+      payload,
+      process.env.REFRESH_TOKEN_SECRET!,
+      {
+        expiresIn: "5 days",
+      }
+    );
+
+    const options = {
+      httpOnly: true,
+    };
+
+    // Set cookie
+    res.cookie("refresh-token", refreshToken, options);
+
+    // Send accessToken
+    return res.status(200).json({
+      id: user._id,
+      username: user.username,
+      token: accessToken,
+    });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      error: "Server error",
+    });
   }
 });
 
--- utils/validation.ts
+++ utils/validation.ts
@@ -1,3 +1,5 @@
+import { RequestHandler } from "express";
+
 import { RegisterUser } from "../interfaces";
 
 export const registrationValidation = ({
@@ -5,14 +7,30 @@ export const registrationValidation = ({
   email,
   password,
 }: RegisterUser) => {
-  if (username && (username.length < 6 || username.length > 32))
+  if (!username || username.length < 6 || username.length > 32)
     return "Invalid username";
   if (
+    !email ||
     !email.match(
       /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     )
   )
     return "Invalid email";
-  if (!password.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/))
+  if (
+    !password ||
+    !password.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)
+  )
     return "Invalid password type. Must contain minimum eight characters, at least one letter and one number.";
+};
+
+export const loginValidation: RequestHandler = (req, res, next) => {
+  if (!req.body.username && !req.body.email)
+    return res.status(400).json({
+      error: "Enter a valid username or email",
+    });
+  if (!req.body.password)
+    return res.status(400).json({
+      error: "Enter a valid password",
+    });
+  next();
 };
