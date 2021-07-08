dd4a7ca31d5d288b06337b4a407ca178ad7fd3a4
name-hansel <snehal6mane@gmail.com>
2021-07-08 22:39:57 +0530

    Cookies sent by localhost client work

--- server.ts
+++ server.ts
@@ -15,7 +15,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
