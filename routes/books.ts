bf04d7902cb87471ba4862ae1ad016269f4a3245
name-hansel <snehal6mane@gmail.com>
2021-07-11 23:27:42 +0530

    Route to get data by book id

--- routes/books.ts
+++ routes/books.ts
@@ -8,7 +8,7 @@ import { bookClient } from "../config/redis";
 // @route   GET /book/search?term=hunger+games&number=5
 // @desc    Search for books
 // @access  Public
-router.get("/search", async (req, res, next) => {
+router.get("/search", async (req, res) => {
   const term = req.query.term?.toString();
   const number = req.query.number ? req.query.number : 5;
   if (!term)
@@ -51,6 +51,69 @@ router.get("/search", async (req, res, next) => {
   } catch (err) {
     return res.status(500).json({
       error: "Server Error",
+    });
+  }
+});
+
+// @route   GET /book/search/:id
+// @desc    Search for book by id
+// @access  Public
+router.get("/search/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!id)
+    return res.status(400).json({
+      error: "No id specified",
+    });
+
+  // Check if present in cache
+  const data = await bookClient.get(id);
+  if (data !== null)
+    return res.status(200).json({
+      books: JSON.parse(data),
+    });
+
+  try {
+    const {
+      data: {
+        volumeInfo: {
+          title,
+          subtitle,
+          authors,
+          publisher,
+          publishedDate,
+          description,
+          pageCount,
+          categories,
+          averageRating,
+          ratingsCount,
+          imageLinks,
+        },
+      },
+    } = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes/${id}?key=${process.env.GOOGLE_BOOKS_KEY}`
+    );
+
+    const book = {
+      title,
+      subtitle: subtitle ? subtitle : "",
+      authors,
+      publisher,
+      publishedDate,
+      description,
+      pageCount,
+      categories,
+      averageRating,
+      ratingsCount,
+      imageUrl: imageLinks ? imageLinks.thumbnail : "",
+    };
+
+    // Set cache
+    bookClient.setex(id, 60 * 60 * 24 * 5, JSON.stringify(book));
+    return res.status(200).json(book);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      error: "Server error",
     });
   }
 });
