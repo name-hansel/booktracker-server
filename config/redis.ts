9451499337e2e9485227a28997a9b5634c963e66
name-hansel <snehal6mane@gmail.com>
2021-07-11 15:38:31 +0530

    Cache book search in redis

--- config/redis.ts
+++ config/redis.ts
@@ -2,6 +2,7 @@ import Redis from "ioredis";
 
 // Connect to redis
 const client = new Redis();
+export const bookClient = new Redis();
 console.log("Redis is running...");
 
 export default client;
--- routes/books.ts
+++ routes/books.ts
@@ -3,16 +3,24 @@ import axios from "axios";
 
 const router = express.Router();
 import { Book } from "../interfaces";
+import { bookClient } from "../config/redis";
 
 // @route   GET /book/search?term=hunger+games&number=5
 // @desc    Search for books
 // @access  Public
-router.get("/search", async (req, res) => {
-  const term = req.query.term;
+router.get("/search", async (req, res, next) => {
+  const term = req.query.term?.toString();
   const number = req.query.number ? req.query.number : 5;
   if (!term)
     return res.status(400).json({
       error: "Please enter a search term",
+    });
+
+  // Check if present in cache
+  const data = await bookClient.get(term);
+  if (data !== null)
+    return res.status(200).json({
+      books: JSON.parse(data),
     });
 
   try {
@@ -35,11 +43,12 @@ router.get("/search", async (req, res) => {
       };
     });
 
+    // Set cache
+    bookClient.setex(term, 60 * 60 * 24 * 5, JSON.stringify(books));
     return res.status(200).json({
       books,
     });
   } catch (err) {
-    console.log(err.message);
     return res.status(500).json({
       error: "Server Error",
     });
