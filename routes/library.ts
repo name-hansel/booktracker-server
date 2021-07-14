aba34d3891189fdbed21ad76f76a537eddb0367f
name-hansel <snehal6mane@gmail.com>
2021-07-14 17:39:06 +0530

    Fixed get library route

--- routes/library.ts
+++ routes/library.ts
@@ -7,18 +7,26 @@ import Library from "../models/Library";
 
 // @route   GET /library/:id
 // @desc    Get library by userid
-// @access  Public
+// @access  Private
 router.get("/:userId", auth, async (req: CustomRequest, res) => {
   const userId = req.params.userId;
   try {
-    const { library } = await Library.findOne({ user: userId });
+    const { user, books } = await Library.findOne({ user: userId });
 
-    if (req.id && req.id !== library.user)
+    if (req.id && req.id != user)
       return res.status(403).json({
         error: "Forbidden resource",
       });
 
-    return res.status(200).json(library.books);
+    if (req.accessToken)
+      return res.status(200).json({
+        library: books,
+        accessToken: req.accessToken,
+      });
+
+    return res.status(200).json({
+      library: books,
+    });
   } catch (err) {
     console.error(err.message);
     return res.status(500).json({
